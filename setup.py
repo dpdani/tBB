4d2777ca09b287dd2e9ed799c6761a8871a0e3fe
Daniele Parmeggiani <dani.parmeggiani@gmail.com>
2016-09-18 19:47:51 +0200

    tBB can now be properly packaged with setuptools.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='tBB',
+    version='0.1b0',
+    description='A network monitoring tool written in Python.',
+    long_description=open('README.md').read(),
+    license='GPLv3',
+    url='https://github.com/dpdani/tBB',
+    author='Daniele Parmeggiani',
+    author_email='dani.parmeggiani@gmail.com',
+    packages=['tBB'],
+    platforms=['POSIX'],
+    install_requires=['aiohttp>=0.22.2'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    keywords='network security',
+    data_files=[
+        ('certs', 'certs/key.pem'),
+        ('certs', 'certs/cert.pem'),
+    ],
+    entry_points={
+        'console_scripts': [
+            'tBB=tBB.main'
+        ]
+    }
+)
